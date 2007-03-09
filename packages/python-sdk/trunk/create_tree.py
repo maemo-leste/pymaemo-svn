c5edb6adf51a677693cb524dc48bdb3671d55ec5
lucianomw <lucianomw@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-03-09 00:24:42 +0000

    -right construction of base tree
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@60 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/python-sdk/trunk/create_tree.py
+++ packages/python-sdk/trunk/create_tree.py
@@ -13,22 +13,28 @@ def download_from_svn(config, section):
     status = os.popen('svn co ' + svn_url)
     print status.read()
 
-    if config.has_option(section, 'package_url'):
-        old_dir_name = config.get(section, 'package_url') 
+    if config.has_option(section, 'source_url'):
+        old_dir_name = 'debian'
     else:
         old_dir_name = 'trunk'
     new_dir_name = section
     status = os.popen('mv ' + old_dir_name + ' ' + new_dir_name)
     print status.read()
 
+def download_source_package(config, section):
+    package_name = config.get(section, 'package_name')
+    source_url = config.get(section, 'source_url') + package_name
+    old_dir_name = config.get(section, 'source_dir_name')
+    new_dir_name = section
+
+    status = os.popen('wget -c -nc ' + source_url+' && tar xf '+package_name+' && mv '+old_dir_name+' '+new_dir_name)
+
 def create_tree(config):
     for section in config.sections():
         print section
-        if config.has_option(section, 'package_url'):
-            for option in config.options(section):
-                print ' ', option, '=', config.get(section, option)
-        else:
-            download_from_svn(config, section)
+        if config.has_option(section, 'source_url'):
+            download_source_package(config, section)
+        download_from_svn(config, section)
 
 def main():
     config = read_cfg_file("packages.ini")   
