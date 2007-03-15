84403be9c996c5630c5571d5260c92ac11aa0537
lucianomw <lucianomw@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-03-15 19:35:21 +0000

    -global vars to keep dir names, some cleanups and some error tests
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@74 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/python-sdk/trunk/create_tree.py
+++ packages/python-sdk/trunk/create_tree.py
@@ -3,6 +3,12 @@ import string
 import os
 import sys
 
+#Some global definitions
+config_file = 'packages.ini'
+sources_dir = 'source_packages'
+debs_dir = 'deb_packages'
+
+
 #TODO: verify error conditions
 
 def read_cfg_file(config_file):
@@ -18,49 +24,56 @@ def download_from_svn(config, section):
        After downloading, the directory is moved inside source tree (if its just patch) or the directory
        is renamed to the correct module name.'''
 
-    if os.path.exists(section):
-        print 'There is a(n) section package already created!'
+    if config.has_option(section, 'source_url'):
+        old_dir_name = 'debian'
+        path_to_test = 'debian/patches'
+    else:
+        old_dir_name = 'trunk'
+        path_to_test = section
+        
+    if os.path.exists(path_to_test):
+        print 'There is a(n) %s package already created!' % (section)
         return
 
     svn_url = config.get(section, 'svn_url')
     status = os.popen('svn co ' + svn_url)
     print status.read()
 
-    if config.has_option(section, 'source_url'):
-        old_dir_name = 'debian'
-    else:
-        old_dir_name = 'trunk'
     new_dir_name = section
     status = os.popen('mv ' + old_dir_name + ' ' + new_dir_name)
     print status.read()
 
 def download_source_package(config, section):
-    '''Download tar.gz files from websites listed in config file.'''
+    '''Download tar.gz files from websites listed in config file and put them in source_packages directory'''
 
     package_name = config.get(section, 'package_name')
     source_url = config.get(section, 'source_url') + package_name
     old_dir_name = config.get(section, 'source_dir_name')
     new_dir_name = section
 
-    status = os.popen('wget -c -nc '+source_url+' && tar xf '+package_name+' && mv '+old_dir_name+' '+new_dir_name)
-#    status = os.popen('mv '+package_name+' sources && ln -s sources/+'package_name'+' orig')
+    status = os.popen('cd '+sources_dir+' && wget -c -nc '+source_url+' && tar xf '+package_name+' && mv '+old_dir_name+' ..\\'+new_dir_name)
+    print status.read()
 
 def apply_patches(package_name):
     '''Apply the patches when necessary'''
 
     status = os.popen('cd '+package_name+' && quilt push -a --quiltrc ../quiltrc')
+    print status
 
 def compile(config):
     '''Create all deb files'''
 
     for section in config.sections():
-        status = os.popen('cd '+section+' && echo dpkg && dpkg-buildpackage -rfakeroot -sa -tc -I.pc -i.svn -us -uc')
+        status = os.popen('cd '+section+' && dpkg-buildpackage -rfakeroot -sa -tc -I.pc -i.svn -us -uc')
         print status.read()
 
 def create_tree(config):
     '''Process all packages listed in config file, to create the source tree, patch it and generate debian packages.'''
 
-    status = os.popen('mkdir sources')
+    if not os.path.exists(sources_dir):
+        status = os.popen('mkdir '+sources_dir)
+        print status
+
     for section in config.sections():
         print section
         apply = 'No'
@@ -74,7 +87,7 @@ def create_tree(config):
 def main():
     '''Construct python for maemo source tree, with all modules contained in packages.ini config file'''
 
-    config = read_cfg_file('packages.ini')   
+    config = read_cfg_file(config_file)
     create_tree(config)
     compile(config)
 
