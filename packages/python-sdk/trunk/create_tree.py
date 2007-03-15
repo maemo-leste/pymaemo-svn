8281d5de9cba9de482f4e29cbace176598e4d504
lucianomw <lucianomw@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-03-15 14:32:02 +0000

    -comments added
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@73 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/python-sdk/trunk/create_tree.py
+++ packages/python-sdk/trunk/create_tree.py
@@ -3,12 +3,21 @@ import string
 import os
 import sys
 
+#TODO: verify error conditions
+
 def read_cfg_file(config_file):
+    '''Read ini file that contains all the packages to be generated, ordered by dependencies.'''
+
     config = ConfigParser()
     config.read(config_file)
     return config
 
 def download_from_svn(config, section):
+    '''Download all necessary information from svn repository. Modules can contain only patches or the
+       entire source tree (only projects without source.tar.gz in another site - e.g.: python-hildon).
+       After downloading, the directory is moved inside source tree (if its just patch) or the directory
+       is renamed to the correct module name.'''
+
     if os.path.exists(section):
         print 'There is a(n) section package already created!'
         return
@@ -26,6 +35,8 @@ def download_from_svn(config, section):
     print status.read()
 
 def download_source_package(config, section):
+    '''Download tar.gz files from websites listed in config file.'''
+
     package_name = config.get(section, 'package_name')
     source_url = config.get(section, 'source_url') + package_name
     old_dir_name = config.get(section, 'source_dir_name')
@@ -35,14 +46,20 @@ def download_source_package(config, section):
 #    status = os.popen('mv '+package_name+' sources && ln -s sources/+'package_name'+' orig')
 
 def apply_patches(package_name):
+    '''Apply the patches when necessary'''
+
     status = os.popen('cd '+package_name+' && quilt push -a --quiltrc ../quiltrc')
 
 def compile(config):
+    '''Create all deb files'''
+
     for section in config.sections():
         status = os.popen('cd '+section+' && echo dpkg && dpkg-buildpackage -rfakeroot -sa -tc -I.pc -i.svn -us -uc')
         print status.read()
 
 def create_tree(config):
+    '''Process all packages listed in config file, to create the source tree, patch it and generate debian packages.'''
+
     status = os.popen('mkdir sources')
     for section in config.sections():
         print section
@@ -55,6 +72,8 @@ def create_tree(config):
             apply_patches(section)
 
 def main():
+    '''Construct python for maemo source tree, with all modules contained in packages.ini config file'''
+
     config = read_cfg_file('packages.ini')   
     create_tree(config)
     compile(config)
