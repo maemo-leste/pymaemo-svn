254feb8d2fae0c906b5917289a3084fa76c3089d
lucianomw <lucianomw@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-03-19 18:28:31 +0000

    -new texts and testing error condition
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@88 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/python-sdk/trunk/create_tree.py
+++ packages/python-sdk/trunk/create_tree.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2006 INdT.
+#
+# Contact: Luciano Miguel Wolf <luciano.wolf@indt.org.br>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+
 from ConfigParser import *
 import string
 import os
@@ -86,7 +105,12 @@ def compile(config):
         print status.read()
         status = os.popen('dpkg -i *.deb')
         print status.read()
+        if not os.path.exists('*.deb'):
+            print "ERROR INSTALLING %s!"%(module)
+            return
         status = os.popen('mv *.dsc *.changes *.tar.gz *.deb '+debs_dir+'/'+arch_dir)
+        print status.read()
+        status = os.popen('mv '+debs_dir+'/'+arch_dir+'*.orig.tar.gz *.deb .')
         print status.read()
     #TODO: Test if deb package has been generated. If not, stop the 'for' loop.
 
