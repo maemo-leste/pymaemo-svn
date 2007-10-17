b389bc034987595124184dfbe43b3b28ea4cd118
lmoura <lmoura@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2007-10-17 19:34:10 +0000

    Improving the example to work with functions
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@573 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/pygtk/trunk/examples/tap/tap_example.py
+++ packages/pygtk/trunk/examples/tap/tap_example.py
@@ -3,41 +3,87 @@
 import gtk
 import hildon
 
-def cb(*args, **kargs):
-    print args
-    print kwargs
 
-def foo(menu):
-    print menu
-    x, y = menu.get_pointer()
 
-    return (x+10, y+10,False)
 
-def main():
-    print "gtk.Widget tap and hold members: "
-    print [x for x in dir(gtk.Widget) if 'tap' in x]
+class Prog(object):
 
-    window = hildon.Window()
-    window.connect("destroy", gtk.main_quit)
+    def __init__(self):
+        object.__init__(self)
+        self.window = hildon.Window()
+        self.window.connect("destroy", gtk.main_quit)
+        
+        menu = gtk.Menu()
+        for i in range(3):
+            item = gtk.MenuItem("Item %d" % i)
+            menu.append(item)
+            item.show()
+        
+        menu.show()
 
-    menu = gtk.Menu()
-    for i in range(3):
-        item = gtk.MenuItem("Item %d" % i)
-        menu.append(item)
-        item.connect("activate", cb)
-        item.show()
+        vbox = gtk.VBox()
 
-    menu.show()
+        hboxX = gtk.HBox()
+        hboxX.pack_start(gtk.Label("X coord"), False, False)
+        self.spin_x = gtk.SpinButton(gtk.Adjustment(lower=-100,upper=100, step_incr=5))
+        hboxX.pack_start(self.spin_x, True, True)
+        vbox.pack_start(hboxX)
 
-    label = gtk.Button("Tap test")
+        hboxY = gtk.HBox()
+        hboxY.pack_start(gtk.Label("Y coord"), False, False)
+        self.spin_y = gtk.SpinButton(gtk.Adjustment(lower=-100,upper=100, step_incr=5))
+        hboxY.pack_start(self.spin_y, True, True)
+        vbox.pack_start(hboxY)
 
-    label.tap_and_hold_setup(menu=menu, callback=foo)
-    print menu.get_attach_widget()
-    window.add(label)
+        self.check_push = gtk.CheckButton(label="Push-In")
+        vbox.pack_start(self.check_push)
 
-    window.show_all()
+        self.button = gtk.Button("Tap test")
+        self.button.tap_and_hold_setup(menu=menu, callback=self.cb)
+        vbox.pack_start(self.button)
+    
+        self.window.add(vbox)
 
-    gtk.main()
+    def cb(self, menu, widget=None, data=None):
+
+        print menu
+        print widget
+        print data
+
+        req_w, req_h = menu.size_request()
+
+        top = widget.get_toplevel()
+        w_x, w_y = top.window.get_origin()
+
+        point_x, point_y, mods = top.window.get_pointer()
+
+        menu_x = point_x + w_x
+        menu_y = point_y + w_y - req_h
+        
+        #if widget.get_direction == gtk.TEXT_DIR_RTL:
+        #    menu_x = menu_x + widget.allocation.width - req_w
+
+        scr_w = widget.get_screen().get_width()
+
+        if (menu_x < w_x):
+            menu_x = w_x
+        elif (menu_x + req_w) > scr_w:
+            menu_x -= ((menu_x + req_w) - scr_w)
+
+        if menu_y < w_y:
+                meny_y = w_y
+
+        menu_x += int(self.spin_x.get_value())
+        menu_y += int(self.spin_y.get_value())
+
+        print "Setting menu at (%d, %d, %d)" % (menu_x, menu_y, self.check_push.get_active())
+
+        return menu_x, menu_y, self.check_push.get_active()
+
+    def run(self):
+        self.window.show_all()
+        gtk.main()
 
 if __name__ == "__main__":
-    main()
+    prog = Prog()
+    prog.run()
