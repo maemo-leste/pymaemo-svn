0e207030237ec4995431beb9b53b4853ec67b4d9
renatofilho <renatofilho@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-06-03 13:57:49 +0000

    initial fremantle documentation
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1031 f8ba266b-2117-0410-b6ba-c3dd9a426789

---
+++ www_fremantle/src/build_pages.py
@@ -0,0 +1,37 @@
+import fileinput
+
+html_pages = [
+              'manual_installation',
+              'installation',
+              'index',
+              'pict1',
+              'pict2',
+              'pict3',
+              'pict4',
+              'pict5',
+              'pict6',
+              'pymaemo25_releasenotes',
+              'sdk_installation'
+             ]
+
+def inject_page (dest_file, page):
+    src_file = open(page + '.html', 'r')
+
+    line = src_file.readline()
+    while len(line) != 0:
+        dest_file.write(line)
+        line = src_file.readline()
+    dest_file.write('\n')
+
+def build_page (page):
+    dest_file = open('../' + page + '.html', 'w')
+
+    for line in fileinput.input("template.html"):
+        if line.find("<!-- Actual content must be injected here [DO NOT CHANGE THIS LINE!] -->") != -1:
+            inject_page(dest_file, page)
+        else:
+            dest_file.write(line)
+
+
+for page in html_pages:
+    build_page (page)
