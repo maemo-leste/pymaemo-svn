e61f52d7b71fc2a63537924e4a1d4e1be6dfe4ed
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-06-03 20:11:54 +0000

    www: commit first examples for python-hildon tutorial example
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1032 f8ba266b-2117-0410-b6ba-c3dd9a426789

---
+++ www/documentation/python_hildon_tutorial/examples/hildon-app-menu-example.py
@@ -0,0 +1,86 @@
+# Based on C code from:
+# "Hildon Tutorial" version 2009-04-28
+# Example 3.1, "Example of a Hildon application menu"
+
+import sys
+
+import gtk
+import hildon
+
+def menu_button_clicked(button, label):
+    buttontext = button.get_label()
+    text = "Last option selected:\n%s" % buttontext
+    label.set_text(text)
+    print >>sys.stderr, "Button clicked: %s" % buttontext
+
+def create_menu(label):
+    menu = hildon.AppMenu()
+
+    for i in xrange(1, 6):
+        # Create menu entries
+        # FIXME: hildon_gtk_button_new() is not exposed to python, so using a
+        # plain gtk.Button for now
+        #button = hildon.GtkButton(gtk.HILDON_SIZE_AUTO)
+        button = gtk.Button()
+        command_id = "Menu command %d" % i
+        button.set_label(command_id)
+
+        # Attach callback to clicked signal
+        button.connect("clicked", menu_button_clicked, label)
+
+        # Add entry to the view menu
+        menu.append(button)
+
+    # Create filters
+    # FIXME: hildon_gtk_radio_button_new() is not exposed to Python, using a
+    # plain gtk.RadioButton for now
+    #button = hildon.GtkRadioButton(gtk.HILDON_SIZE_AUTO, None)
+    button = gtk.RadioButton()
+    button.set_label("filter one")
+    button.connect("clicked", menu_button_clicked, label)
+    menu.add_filter(button)
+    button.set_mode(False)
+
+    button = gtk.RadioButton(button)
+    button.set_label("filter two")
+    button.connect("clicked", menu_button_clicked, label)
+    menu.add_filter(button)
+    button.set_mode(False)
+
+    menu.show_all()
+
+    return menu
+
+def main():
+    win = hildon.StackableWindow()
+
+    # Create and pack labels
+    label = gtk.Label("This is an example of the\nHildonAppMenu widget.\n\n"
+                      "Click on the titlebar\nto pop up the menu.")
+    label2 = gtk.Label("No menu option has been selected yet.")
+
+    label.set_justify(gtk.JUSTIFY_CENTER)
+    label2.set_justify(gtk.JUSTIFY_CENTER)
+
+    vbox = gtk.VBox(False, 10)
+
+    vbox.pack_start(label, True, True, 0)
+    vbox.pack_start(label2, True, True, 0)
+
+    # Create menu
+    menu = create_menu(label2)
+
+    # Attach menu to the window
+    win.set_app_menu(menu)
+
+    # Add label's box to window
+    win.add(vbox)
+
+    win.connect("delete_event", gtk.main_quit)
+
+    win.show_all()
+
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
---
+++ www/documentation/python_hildon_tutorial/examples/hildon-hello-word.py
@@ -0,0 +1,52 @@
+# Based on C code from:
+# "Hildon Tutorial" version 2009-04-28
+# Example 1.2, "Hildon Hello World program"
+
+import gtk
+import hildon
+
+# This is a callback function. The data arguments are ignored in this example.
+def hello(widget, data):
+    print "Hello World!"
+
+def main():
+    # Get an instance of HildonProgram. It is an object used to represent an
+    # application running in the Hildon framework.
+    program = hildon.hildon_program_get_instance()
+
+    # create a new hildon window
+    window = hildon.Window()
+
+    # Registers a window as belonging to the program
+    program.add_window(window)
+
+    # When the window is given the "delete_event" signal (this is given by the
+    # window manager, usually by the "close" option, or on the titlebar), we
+    # ask it to call the delete_event () function as defined above. The data
+    # passed to the callback function is None and is ignored in the callback
+    # function.
+    window.connect("delete_event", gtk.main_quit, None)
+
+    # FIXME: hildon_button_new_with_text missing (MB#4629)
+    #button = hildon.Button(hgtk.HILDON_SIZE_AUTO, hildon.BUTTON_ARRANGEMENT_VERTICAL, "Hello world!")
+    button = hildon.Button(gtk.HILDON_SIZE_AUTO, hildon.BUTTON_ARRANGEMENT_VERTICAL)
+    button.set_title("Hello world!")
+
+    # When the button is given the "clicked" signal, we ask it to call the
+    # hello () function as defined above. The data passed to the callback
+    # function is None and is ignored in the callback function.
+    button.connect("clicked", hello, None)
+
+    # This packs the button into the window (a GTK+ container).
+    window.add(button)
+
+    # The final step is to display this newly created widget and all widgets it
+    # contains.
+    window.show_all()
+
+    # All GTK+ applications must have a gtk_main(). Control ends here and waits
+    # for an event to occur (like a key press or mouse event).
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
