286e4c05f7e382a66f47ff687c4070cd4321a971
renatofilho <renatofilho@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-06-05 22:06:28 +0000

    porte 5.* examples.
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1040 f8ba266b-2117-0410-b6ba-c3dd9a426789

---
+++ www/documentation/python_hildon_tutorial/examples/hildon-pannable-area.py
@@ -0,0 +1,54 @@
+# Based on C code from:
+# "Hildon Tutorial" version 2009-04-28
+# Example 5.1, "Example of a pannable area"
+
+import gtk
+import hildon
+
+def create_table():
+
+  # create a table of 10 by 10 squares. 
+  table = gtk.Table (10, 10, False)
+
+  # set the spacing to 10 on x and 10 on y 
+  table.set_row_spacings(10)
+  table.set_col_spacings(10)
+
+  table.show()
+
+  # this simply creates a grid of toggle buttons on the table
+  # to demonstrate the scrolled window. */
+  for i in range(10):
+    for j in range(10):
+      buffer = "button (%d,%d)\n" % (i, j)
+      button = gtk.ToggleButton(buffer)
+      table.attach(button, i, i+1, j, j+1)
+
+  return table;
+
+
+def main():
+  
+
+  window = hildon.StackableWindow()
+
+  # FIXME: probably unnecessary. See MB#4633
+  #window.connect("destroy", test, None)
+
+  pannable_area = hildon.PannableArea();
+
+  table = create_table();
+
+  # pack the table into the scrolled window 
+  pannable_area.add_with_viewport(table)
+
+  # Add the box into the window
+  window.add(pannable_area)
+
+  window.show_all()
+
+  gtk.main()
+
+if __name__ == "__main__":
+  main()
+
