1ca0e770c26925afc1a6566cc95cf6be8a45d6f2
andy80 <andy80@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-07-28 10:18:47 +0000

    python-mafw: added get_sources() unittest
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1257 f8ba266b-2117-0410-b6ba-c3dd9a426789

---
+++ packages/python-mafw/branches/andrea/tests/test_mafw_registry_get_sources.py
@@ -0,0 +1,21 @@
+import unittest
+import gobject
+import mafw
+
+class MyPlugin(mafw.Source):
+	__gtype_name__ = 'MyPlugin'
+
+class TestMafwRegistry(unittest.TestCase):
+    def test_get_instance(self):
+        reg = mafw.Registry.get_instance()
+
+	x1 = gobject.new(MyPlugin, uuid = 'test1')
+	x2 = gobject.new(MyPlugin, uuid = 'test2')
+	
+	reg.add_extension(x1)
+	reg.add_extension(x2)
+	
+        self.assertTrue(reg.get_sources(), [x1, x2])
+
+if __name__ == "__main__":
+    unittest.main()
