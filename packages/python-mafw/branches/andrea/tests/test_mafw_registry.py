e2fc893fb3fbb238ff2c93e14f4d44072cd6b6b7
andy80 <andy80@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-07-28 14:31:03 +0000

    python-mafw: get_sources and get_renderers unittest included in registry unittest
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1261 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- packages/python-mafw/branches/andrea/tests/test_mafw_registry.py
+++ packages/python-mafw/branches/andrea/tests/test_mafw_registry.py
@@ -1,6 +1,30 @@
 import unittest
-
+import gobject
 import mafw
+
+class MySourcePlugin(mafw.Source):
+    __gtype_name__ = 'MySourcePlugin'
+
+class TestMafwRegistry(unittest.TestCase):
+    def test_get_sources(self):
+        reg = mafw.Registry.get_instance()
+        x1 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin1')
+        x2 = gobject.new(MySourcePlugin, uuid = 'MySourcePlugin2')
+        reg.add_extension(x1)
+        reg.add_extension(x2)
+        self.assertTrue(reg.get_sources(), [x1, x2])
+
+class MyRendererPlugin(mafw.Renderer):
+    __gtype_name__ = 'MyRendererPlugin'
+
+class TestMafwRegistry(unittest.TestCase):
+    def test_get_renderers(self):
+        reg = mafw.Registry.get_instance()
+        x1 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin1')
+        x2 = gobject.new(MyRendererPlugin, uuid = 'MyRendererPlugin2')
+        reg.add_extension(x1)
+        reg.add_extension(x2)
+        self.assertTrue(reg.get_renderers(), [x1, x2])
 
 class TestMafwRegistry(unittest.TestCase):
     def test_get_instance(self):
