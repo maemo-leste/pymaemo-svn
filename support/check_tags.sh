dce63c21084f7c0a71be251c6e4893a9c9a3690e
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-07-31 18:10:28 +0000

    support: fix check_tags.sh script
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1288 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- support/check_tags.sh
+++ support/check_tags.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # vim: ts=4 sw=4 et ai sta
+# Check which package releases were uploaded to Maemo extras, but do not have
+# release tags.
+# It must be run from a git-svn copy of the PyMaemo tree.
 set -e -u
 
-if [ ! -d packages/ ]; then
-    echo "ERROR: this script must be run from PyMaemo repository"
+if [ ! -d packages/ -o ! -d .git/ -o ! -d .git/svn/ ]; then
+    echo "ERROR: this script must be run from a PyMaemo git-svn repository" >&2
     exit 1
 fi
 
 tempdir=$(mktemp -d)
-trap "echo Removing temporary directory...; rm -rf $tempdir" EXIT
+trap "echo Removing temporary directory... >&2; rm -rf $tempdir" EXIT
 trap "exit 1" INT
 
 function find_dsc_url()
@@ -28,8 +31,8 @@ function find_commit()
     dsc_url=$3
 
     if [ ! -f $tempdir/${pkg}_${ver}.dsc ]; then
-        echo "Downloading source for $pkg..." >&2
-        (cd $tempdir && dget -q -u $dsc_url >&2 2>/dev/null)
+        echo "Downloading source for $pkg ($ver)..." >&2
+        (cd $tempdir && dget -q -u $dsc_url &>/dev/null)
     fi
 
     src_dir=$(ls -d $tempdir/*/ | head -n1) && test -n "$src_dir"
@@ -42,7 +45,6 @@ function find_commit()
         new=packages/$pkg/trunk/debian
     fi
     commits=$(git log --pretty=oneline $new | awk '{print $1}')
-    commit=
     git log --pretty=oneline $new | awk '{print $1}' | while read c; do
         #echo "Trying commit $c..." >&2
         git archive $c $new | tar -C $tempdir -xf-
@@ -55,19 +57,19 @@ function find_commit()
 }
 
 wget -P $tempdir http://repository.maemo.org/extras-devel/dists/fremantle/free/source/Sources.bz2
-#for d in packages/*/; do
-for d in packages/python-hildon/; do
-    pkg=$(basename $d)
+for d in packages/*/trunk/debian/; do
+    pkg=$(basename $(readlink -f $d/../../))
     bzcat $tempdir/Sources.bz2 | grep-dctrl -XnSs Version $pkg | while read v; do
         if [ ! -d "packages/$pkg/tags/$v" ]; then
-            echo "ERROR: $pkg: tag for version \"$v\" does not exist"
+            echo "ERROR: $pkg: tag for version \"$v\" does not exist" >&2
             dsc_url=$(find_dsc_url http://repository.maemo.org/extras-devel $tempdir/Sources.bz2 $pkg $v)
             c=$(find_commit $pkg $v $dsc_url)
             if [ -z "$c" ]; then
-                echo "ERROR: commit not found for $pkg $v" >&2
+                echo "ERROR: commit not found for $pkg ($v)" >&2
             else
-                git log -n1 $c | awk '/git-svn-id:/{print $2}' | cut -d@ -f2
-                #echo "svn cp packages/$pkg/trunk packages/$pkg/tags/$v"
+                rev=$(git log -n1 $c | awk '/git-svn-id:/{print $2}' | cut -d@ -f2)
+                echo "svn cp -r $rev packages/$pkg/trunk packages/$pkg/tags/$v"
+                echo "svn commit -m \"$pkg: tag release $v\" packages/$pkg/tags/$v"
             fi
         fi
     done
