a1778a33d2113b86a9fd4399bc1e8ca2ea0b682b
hugo_pl <hugo_pl@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-10-15 16:14:45 +0000

    Fixed links to hildon tutorials and manuals
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1392 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- sphinx/www/conf.py
+++ sphinx/www/conf.py
@@ -22,10 +22,12 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.refcounting', 'screenshots']
+extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.refcounting', 'sphinx.ext.intersphinx', 'screenshots']
 
 rst_epilog = """
 """
+
+intersphinx_mapping = {'./python_hildon_tutorial': '../python_hildon_tutorial/_build/html/objects.inv'}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
