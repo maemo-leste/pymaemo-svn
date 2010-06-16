0419f72b5b325e69efd0cdc68b67975fbf4545be
wmoreira <wmoreira@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2010-06-16 23:06:28 +0000

    Update stdeb_example
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1488 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- www/stdeb_example/myscript-app/setup.py
+++ www/stdeb_example/myscript-app/setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(
     name='myscript',
+    version='0.1',
     author='Lauro Moura',
     author_email='lauro.neto@donotspam.org',
     scripts=['myscript'],
